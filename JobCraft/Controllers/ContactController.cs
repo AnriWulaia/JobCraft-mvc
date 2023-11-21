951908429b12247243ff0e12236b5ac0a8d66590
AnriWulaia <wulaia.anri@gmail.com>
2023-11-21 20:57:00 +0400

    added contact page

---
+++ JobCraft/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobCraft.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
